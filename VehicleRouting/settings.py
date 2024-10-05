135511eb76448903106c362782e3428a84b11075
sagar shird <47175369+midriffdeveloper1@users.noreply.github.com>
2024-10-05 18:33:38 +0530

    --

--- VehicleRouting/settings.py
+++ VehicleRouting/settings.py
@@ -157,13 +157,15 @@ DEFAULT_FROM_EMAIL = env("EMAIL_HOST_USER")
 
 GEMINI_API_KEY=env("GEMINI_API_KEY")
 GOOGLEMAPSKEY=env('GOOGLEMAPSKEY')
+FROMLATITUDE=28.673659
+FROMLONGITUDE=77.140037
 
 
 
 
 CRONJOBS = [
    ('0 0 * * *', 'home.cron.today_due_emails'),   # every morning at 12:00 AM for due payemnts
-   ('0 0 * * *', 'home.cron.overdue_payments_emails'),   # every morning at 12:00 AM
+   ('0 0 * * *', 'home.cron.due_payments_emails'),   # every morning at 12:00 AM
    ('0 0 * * *', 'home.cron.final_warning_emails'),   # every morning at 12:00 AM
 
    
