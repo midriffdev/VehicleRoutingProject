26634fc2285c2c01910248ed1421cbb9c6b3d5a5
midriffdev <mimailto:i.midriff@gmail.com>
2024-10-05 11:24:51 +0530

    cron

--- home/cron.py
+++ home/cron.py
@@ -15,6 +15,13 @@ from django.conf import settings #for email send host name
 def today_due_emails():
     orders = Order.objects.filter(order_status="delivered", payment_status="due", due_payment_date__date=datetime.datetime.now().date())
     for i in orders:
+
+        content=f"Your payment for Order {i.product_name} is due today. Please settle it by the end of the day to avoid any interruptions in service. Thank you!"
+
+        html_message = render_to_string('home/orderemail.html', {'user': i.email,'content':content})
+        try: send_mail('Payment Due Today for Your Order', strip_tags(html_message), settings.EMAIL_HOST_USER, [i.email,], html_message=html_message)
+        except Exception as e: print("\n\n______________________unable to send mail", e)
+
         Notifications.objects.create(
             content=f"Your payment for Order {i.product_name} is due today. Please settle it by the end of the day to avoid any interruptions in service. Thank you!",
             title=f'Payment Due Today for Your Order {i.product_name}',
@@ -24,11 +31,19 @@ def today_due_emails():
 
 def due_payments_emails(): #  completed first payment
     orders=Order.objects.filter(order_status="delivered",payment_status="due", due_payment_date__date__lt=datetime.datetime.now().date())
+
     for i in orders:
+
+        content=f"Your payment for Order {i.product_name} is overdue; please settle it as soon as possible to avoid service interruptions. Thank you!"
+
+        html_message = render_to_string('home/orderemail.html', {'user': i.email,'content':content})
+        try: send_mail(f'Urgent: Payment Pending for Your Order {i.product_name}.', strip_tags(html_message), settings.EMAIL_HOST_USER, [i.email,], html_message=html_message)
+        except Exception as e: print("\n\n______________________unable to send mail", e)
+
+        Notifications.objects.create(content=f"Your payment for Order {i.product_name} is overdue; please settle it as soon as possible to avoid service interruptions. Thank you!", title=f'Payment Due for Your Order {i.product_name}.', receiver=i)
+
         i.payment_status="past_due"
         i.save()
-        Notifications.objects.create(content=f"Your payment for Order {i.product_name} is overdue; please settle it as soon as possible to avoid service interruptions. Thank you!", title=f'Payment Due for Your Order {i.product_name}.', receiver=i,)
-
 
 
 def final_warning_emails():
@@ -38,8 +53,9 @@ def final_warning_emails():
     orders = Order.objects.filter(order_status="delivered", payment_status="past_due", due_payment_date__date__lt=five_days_ago)
     
     for i in orders:
-        html_message = render_to_string('home/orderemail.html', {'user': i.email})
-        try: send_mail('Order delivered to you successfully.', strip_tags(html_message), settings.EMAIL_HOST_USER, [i.email,], html_message=html_message)
+        content=f"Dear Customer, your payment for Order {i.product_name} has been pending for more than 5 days. Please settle it immediately to avoid escalation to our collections department. We value your prompt action in this matter. Thank you!"
+        html_message = render_to_string('home/orderemail.html', {'user': i.email,'content':content})
+        try: send_mail('Urgent: Payment Pending for Your Order.', strip_tags(html_message), settings.EMAIL_HOST_USER, [i.email,], html_message=html_message)
         except Exception as e: print("\n\n______________________unable to send mail", e)
 
         Notifications.objects.create(
