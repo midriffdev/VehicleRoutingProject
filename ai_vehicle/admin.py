6f1cf36553ae9329a497c817c2be9edc5574af85
sagar shird <47175369+midriffdeveloper1@users.noreply.github.com>
2024-10-15 15:03:12 +0530

    --

--- ai_vehicle/admin.py
+++ ai_vehicle/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import GenRoutes, HeadQuarter, Truckdata, routedata
 
 admin.site.register(GenRoutes)
-admin.site.register(HeadQuarter)
+admin.site.register(HeadQuarter, ImportExportModelAdmin)
 admin.site.register(Truckdata)
 admin.site.register(routedata)
 
--- home/admin.py
+++ home/admin.py
@@ -3,14 +3,8 @@ from import_export.admin import ImportExportModelAdmin
 from .models import *
 
 
-admin.site.register(Order)
+admin.site.register(Order, ImportExportModelAdmin)
 admin.site.register(Notifications)
 admin.site.register(Truck, ImportExportModelAdmin)
 admin.site.register(Report_order)
 admin.site.register(ServiceRecord)
-
-
-# class TruckAdmin(ImportExportModelAdmin):
-#     resource_classes = [TruckResource]
-
-# admin.site.register(Truck, TruckAdmin)
